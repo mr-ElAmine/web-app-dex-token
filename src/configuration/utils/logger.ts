bcfc1d35894e493012cac4217568e36419b9b1ed
mr-ElAmine <aissaelamine.arar@gmail.com>
2025-05-05 21:47:02 +0200

    fix(main): fix error logger

--- src/configuration/utils/logger.ts
+++
@@ -1,4 +0,0 @@
-// import { ContainerInstance } from '@/configuration/di/container';
-// import type { LoggerInterface } from '@/domain/interfaces/logger.interface';
-
-// export const logger = ContainerInstance.resolve<LoggerInterface>('LoggerInterface');
